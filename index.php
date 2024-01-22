905ea6f53bb16a1418025d491e4861efa6cd57ae
Y32CODE <85583444+Y32CODE@users.noreply.github.com>
2024-01-22 16:41:29 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World</p>'; ?>
+    <?php
+    echo $_SERVER['REMOTE_ADDR'];
+    ?>
+
+  <!--
+  This script places a badge on your repl's full-browser view back to your repl's cover
+  page. Try various colors for the theme: dark, light, red, orange, yellow, lime, green,
+  teal, blue, blurple, magenta, pink!
+  -->
+  <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
+  </body>
+</html>
